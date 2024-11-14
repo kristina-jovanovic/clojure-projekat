e53e7bc6f93c3b638ec13773f39d5a29bf7938f9
kristina-jovanovic <jovanovick25@gmail.com>
2024-11-14 15:37:38 +0100

    pocetna aplikacija

---
+++ test/clojure_projekat/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-projekat.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-projekat.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
